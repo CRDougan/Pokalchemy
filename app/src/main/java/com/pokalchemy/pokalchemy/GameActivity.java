78e628f9a343469b5c5dc280aaac4e8b069d323f
Jacob Davis <Jacob Davis>
2016-04-10 17:48:15 -0600

    minor fixes

--- app/src/main/java/com/pokalchemy/pokalchemy/GameActivity.java
+++ app/src/main/java/com/pokalchemy/pokalchemy/GameActivity.java
@@ -4,79 +4,88 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.ImageButton;
 
 public class GameActivity extends AppCompatActivity {
 
-	Button pokemon, animals, elements, other;
-	boolean p_on = false, a_on = false, e_on = false, o_on = false;
+	private Button mPokemon, mAnimal, mElement, mOther;
+	private ImageButton mTrash;
+	private boolean p_on = false, a_on = false, e_on = false, o_on = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
 
-		pokemon = (Button)findViewById(R.id.pokemon_button);
-		pokemon.setOnClickListener(new View.OnClickListener() {
+		mPokemon = (Button)findViewById(R.id.pokemon_button);
+		mPokemon.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				if (p_on) {
 					p_on = false;
-					pokemon.setTextColor(getResources().getColor(R.color.pokemon));
-					pokemon.setBackground(getResources().getDrawable(R.color.transparent));
+					mPokemon.setTextColor(getResources().getColor(R.color.pokemon));
+					mPokemon.setBackground(getResources().getDrawable(R.color.transparent));
 				} else {
 					p_on = true;
-					pokemon.setTextColor(getResources().getColor(R.color.colorAccent));
-					pokemon.setBackground(getResources().getDrawable(R.color.pokemon));
+					mPokemon.setTextColor(getResources().getColor(R.color.colorAccent));
+					mPokemon.setBackground(getResources().getDrawable(R.color.pokemon));
 				}
 			}
 		});
 
-		animals = (Button)findViewById(R.id.animals_button);
-		animals.setOnClickListener(new View.OnClickListener() {
+		mAnimal = (Button)findViewById(R.id.animal_button);
+		mAnimal.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				if (a_on) {
 					a_on = false;
-					animals.setTextColor(getResources().getColor(R.color.animals));
-					animals.setBackground(getResources().getDrawable(R.color.transparent));
+					mAnimal.setTextColor(getResources().getColor(R.color.animal));
+					mAnimal.setBackground(getResources().getDrawable(R.color.transparent));
 				} else {
 					a_on = true;
-					animals.setTextColor(getResources().getColor(R.color.colorAccent));
-					animals.setBackground(getResources().getDrawable(R.color.animals));
+					mAnimal.setTextColor(getResources().getColor(R.color.colorAccent));
+					mAnimal.setBackground(getResources().getDrawable(R.color.animal));
 				}
 			}
 		});
 
-		elements = (Button)findViewById(R.id.elements_button);
-		elements.setOnClickListener(new View.OnClickListener() {
+		mElement = (Button)findViewById(R.id.element_button);
+		mElement.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				if (e_on) {
 					e_on = false;
-					elements.setTextColor(getResources().getColor(R.color.elements));
-					elements.setBackground(getResources().getDrawable(R.color.transparent));
+					mElement.setTextColor(getResources().getColor(R.color.element));
+					mElement.setBackground(getResources().getDrawable(R.color.transparent));
 				} else {
 					e_on = true;
-					elements.setTextColor(getResources().getColor(R.color.colorAccent));
-					elements.setBackground(getResources().getDrawable(R.color.elements));
+					mElement.setTextColor(getResources().getColor(R.color.colorAccent));
+					mElement.setBackground(getResources().getDrawable(R.color.element));
 				}
 			}
 		});
 
-		other = (Button)findViewById(R.id.other_button);
-		other.setOnClickListener(new View.OnClickListener() {
+		mOther = (Button)findViewById(R.id.other_button);
+		mOther.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				if (o_on) {
 					o_on = false;
-					other.setTextColor(getResources().getColor(R.color.other));
-					other.setBackground(getResources().getDrawable(R.color.transparent));
+					mOther.setTextColor(getResources().getColor(R.color.other));
+					mOther.setBackground(getResources().getDrawable(R.color.transparent));
 				} else {
 					o_on = true;
-					other.setTextColor(getResources().getColor(R.color.colorAccent));
-					other.setBackground(getResources().getDrawable(R.color.other));
+					mOther.setTextColor(getResources().getColor(R.color.colorAccent));
+					mOther.setBackground(getResources().getDrawable(R.color.other));
 				}
+			}
+		});
+
+		mTrash = (ImageButton)findViewById(R.id.trash);
+		mTrash.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// #TODO delete all ingredients in mixing area
 			}
 		});
 	}
